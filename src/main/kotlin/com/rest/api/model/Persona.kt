c7e1229c72ddc9558524ea8f2e0d3cc8bb39df31
Gaston Saillen <gaston_luque_29@hotmail.com>
2019-09-10 17:34:05 -0300

    Mysql added

--- src/main/kotlin/com/rest/api/model/Persona.kt
+++ src/main/kotlin/com/rest/api/model/Persona.kt
@@ -1,6 +1,5 @@
 package com.rest.api.model
 
-import org.hibernate.annotations.GenericGenerator
 import java.time.LocalDate
 import javax.persistence.*
 
@@ -11,8 +10,8 @@ data class Persona(val dni: Long = 0, val nombre: String = "",
                    val fechaNac: LocalDate? = null) {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id:Long = 0
+
 
 }
--- src/main/kotlin/com/rest/api/model/Titulo.kt
+++ src/main/kotlin/com/rest/api/model/Titulo.kt
@@ -1,6 +1,5 @@
 package com.rest.api.model
 
-import org.hibernate.annotations.GenericGenerator
 import javax.persistence.*
 
 @Entity
@@ -8,7 +7,7 @@ import javax.persistence.*
 data class Titulo(val titulo:String = "") {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id:Long = 0
+
 }
